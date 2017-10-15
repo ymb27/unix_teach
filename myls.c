7f7423774f7e0225129bef9f1e58cd73bcb0c6cf
ymb <ymbx27@gmail.com>
2017-10-16 00:11:04 +0800

    实验2myls

--- myls.c
+++ myls.c
@@ -1,22 +1,54 @@
 #include "apue.h"
+#include <sys/stat.h>
+#include <dirent.h>
+#include <pwd.h>
 
 int
 main(int argc,char *argv[])
 {
-	struct stat buf;
-	
 
+	DIR *dp;
+	struct stat buf;
+	struct dirent *dirp;
+
+	char usr_mode[]="---";
+	
 	if(argv[1] == 0){
 		err_quit("usage: myls directory_name");
 	}
 
-	if(stat(argv[1],&buf) < 0){
-		err_ret("stat error");
-	}
 
-	if(S_ISDIR(buf.st_mode)){
-		
-	}else{
+	if((dp = opendir(argv[1])) == NULL)
+
 		err_quit("error: %s is no a directory",argv[1]);
-	}
+	
+		while((dirp = readdir(dp)) != NULL){
+
+			if(stat(dirp->d_name,&buf) == 0){
+				
+				if(S_ISREG(buf.st_mode)){
+
+					if(buf.st_mode & S_IRUSR)
+						usr_mode[0] = 'r';
+					
+					if(buf.st_mode & S_IWUSR)
+						usr_mode[1] = 'w';
+					
+					if(buf.st_mode & S_IXUSR)
+						usr_mode[2] = 'x';
+					
+
+				printf("%s ",usr_mode);
+
+				printf("%s ",getpwuid(buf.st_uid)->pw_name);
+
+				printf("%s\n",dirp->d_name);
+				}
+			}
+		}
+
+				
+
+	closedir(dp);
+	exit(0);
 }
